6f38316b42169bca280ae594c8fa5dfedbe6b235
Baptiste Rousseau <bapt09.rousseau@gmail.com>
2022-03-05 02:01:43 +0100

    properly working carousel

--- manual.js
+++ manual.js
@@ -1,4 +1,5 @@
-document.getElementById("page").addEventListener('click', function (event) {
+/*
+document.getElementById("page_content").addEventListener('click', function (event) {
     const source = this.src;
     const myRegexp = new RegExp(/(\d{2})(\.[^.]*?)(?=\?|#|$)/);
     const extension = source.match(myRegexp);
@@ -6,16 +7,35 @@ document.getElementById("page").addEventListener('click', function (event) {
     const coords = this.getBoundingClientRect();
     const xPos = event.pageX - coords.left;
 
-    let nextIndex = +extension[1];
-    if ((coords.width / 2) >= xPos && extension[1] > 1) {
-        nextIndex--;
-    } else if ((coords.width / 2) < xPos && extension[1] < 27) {
-        nextIndex++;
+    let nextIndex;
+    if ((coords.width / 2) >= xPos) {
+        nextIndex = ((+extension[1] - 2) + 27) % 27 + 1;
+    } else {
+        nextIndex = (+extension[1] + 1) % 27;
     }
 
     let nextPage = source.replace(myRegexp, "") + n(nextIndex) + extension[2];
     this.src = nextPage;
 });
+*/
+
+function turn(direction) {
+    const page = document.getElementById("page_content");
+    const myRegexp = new RegExp(/(\d{2})(\.[^.]*?)(?=\?|#|$)/);
+    const extension = page.src.match(myRegexp);
+
+    let nextIndex;
+    if (direction == "left") {
+        nextIndex = ((+extension[1] - 2) + 27) % 27 + 1;
+    } else {
+        nextIndex = (+extension[1] + 1) % 27;
+    }
+
+    let nextPage = page.src.replace(myRegexp, "") + n(nextIndex) + extension[2];
+
+    page.src = nextPage;
+    document.getElementById("page_number").innerText = `${nextIndex}/27`
+}
 
 function n(num, len = 2) {
     return `${num}`.padStart(len, '0');
