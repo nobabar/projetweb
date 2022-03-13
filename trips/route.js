c0dc309b2235762058abb0a1c68a3e8c79bcd265
Baptiste Rousseau <bapt09.rousseau@gmail.com>
2022-03-14 00:09:28 +0100

    perfectly working bridge

--- trips/route.js
+++ trips/route.js
@@ -9,12 +9,43 @@ const avg = arr => arr.reduce((acc, v, i, a) => (acc + v / a.length), 0);
 const getDatesBetween = (start, end, n) => {
     const step = Math.ceil((end.getTime() - start.getTime()) / n)
     let dates = [];
-    let current = start;
+    let current = new Date(start);
     for (i = 0; i < n; i++) {
         dates.push(new Date(current));
         current.setTime(current.getTime() + step);
     }
     return dates;
+};
+
+function distLatLon(lat1, lat2, lon1, lon2) {
+    let radlat1 = Math.PI * lat1 / 180,
+        radlat2 = Math.PI * lat2 / 180,
+        radlon1 = Math.PI * lon1 / 180,
+        radlon2 = Math.PI * lon2 / 180;
+
+    let dlon = radlon2 - radlon1,
+        dlat = radlat2 - radlat1;
+
+    let a = Math.pow(Math.sin(dlat / 2), 2)
+        + Math.cos(radlat1)
+        * Math.cos(radlat2)
+        * Math.pow(Math.sin(dlon / 2), 2);
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let dist = 6378 * c
+
+    return dist;
+};
+
+function findClose(latVar, latArr, lonVar, lonArr) {
+    let closeIdx = [];
+
+    for (let i = 0; i < latArr.length; i++) {
+        if (distLatLon(latVar, latArr[i], lonVar, lonArr[i]) <= 5) {
+            closeIdx.push(i);
+        }
+    }
+    return Math.round(avg(closeIdx));
 };
 
 // Fonction d'initialisation de la carte
@@ -64,24 +95,48 @@ window.onload = function () {
     // On ajoute à la carte
     geojsonLayer.addTo(carte);
 
+    let redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    let marker = new L.Marker([
+        lat[Math.round(lat.length / 2)],
+        lon[Math.round(lon.length / 2)]
+    ],
+        {
+            icon: redIcon
+        }).addTo(carte);
+
+    // altitude plot
     let myPlot = document.getElementById("plot");
-
+    let dates = getDatesBetween(startDate, endDate, height.length);
     let data = [{
-        x: getDatesBetween(startDate, endDate, height.length),
-        y: height,
-        type: 'scatter',
-        mode: 'line'
+        x: dates, y: height, name: 'Trace',
+        type: 'scatter', mode: 'line'
+    },
+    {
+        x: [
+            dates[Math.round(dates.length / 2)]
+        ],
+        y: [
+            height[Math.round(height.length / 2)]
+        ],
+        name: 'Marqueur', type: 'scatter', mode: 'markers',
+        marker: { size: 15, color: "#CB2B3E" }
     }];
-
     let layout = {
         hovermode: 'closest',
         xaxis: { type: 'date', title: "Date" },
         yaxis: { title: "Altitude (en mètres)" }
     };
-
     let config = { responsive: true };
 
-    // Display with Plotly
+    // display with Plotly
     Plotly.newPlot("plot", data, layout, config);
 
     myPlot.on("plotly_click", function (data) {
@@ -89,30 +144,23 @@ window.onload = function () {
         const pLat = lat[pn];
         const pLon = lon[pn];
 
-        let redIcon = new L.Icon({
-            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
-        });
+        marker.setLatLng([pLat, pLon]);
 
-        let marker = new L.Marker([pLat, pLon], { icon: redIcon, draggable: false }).addTo(carte);
+        Plotly.restyle(myPlot, {
+            x: [[data.points[0].x]],
+            y: [[data.points[0].y]]
+        }, [1]);
+    });
 
-        marker.on({
-            mousedown: function () {
-                carte.on('mousemove', function (e) {
-                    carte.dragging.disable();
-                    var point = geojsonLayer.closestLayerPoint(carte.latLngToLayerPoint(e.latlng))
-                    marker.setLatLng(carte.layerPointToLatLng(point));
-                });
-            }
-        });
-        carte.on('mouseup', function (e) {
-            carte.removeEventListener('mousemove');
-            carte.dragging.enable();
-        });
+    geojsonLayer.on('click', function (e) {
+        marker.setLatLng(e.latlng);
+
+        let closest = findClose(e.latlng.lat, lat, e.latlng.lng, lon);
+
+        Plotly.restyle(myPlot, {
+            x: [[dates[closest]]],
+            y: [[height[closest]]]
+        }, [1]);
     });
 };
 
