e5e3aa77f4a93844002665bf45ca2d6e518de0c5
Baptiste Rousseau <bapt09.rousseau@gmail.com>
2022-03-20 21:43:23 +0100

    expanded quizz

--- manual/testmeca.js
+++ manual/testmeca.js
@@ -1,20 +1,39 @@
+let guidon_selectors;
+let guidon_options;
 
-let selectors;
-let options;
+let moyeu_selectors;
+let moyeu_options;
 
 window.onload = function () {
-    selectors = document.querySelectorAll("select")
-    options = selectors[0].querySelectorAll('option');
+    guidon_selectors = document.querySelectorAll("#guidon select");
+    guidon_options = guidon_selectors[0].querySelectorAll('option');
 
-    for (let i = 1; i < selectors.length; i++) {
-        for (let opt of options) {
-            selectors[i].appendChild(opt.cloneNode(true));
+    for (let i = 1; i < guidon_selectors.length; i++) {
+        for (let opt of guidon_options) {
+            guidon_selectors[i].appendChild(opt.cloneNode(true));
+        }
+    }
+
+    moyeu_selectors = document.querySelectorAll("#moyeulibre select");
+    moyeu_options = moyeu_selectors[0].querySelectorAll('option');
+
+    for (let i = 1; i < moyeu_selectors.length; i++) {
+        for (let opt of moyeu_options) {
+            moyeu_selectors[i].appendChild(opt.cloneNode(true));
         }
     }
 }
 
 function giveSelection(selected) {
     if (selected.value != "empty") {
+        let selectors, options;
+        if (selected.parentNode.id === "guidon") {
+            selectors = guidon_selectors;
+            options = guidon_options;
+        } else {
+            selectors = moyeu_selectors;
+            options = moyeu_options;
+        }
         for (let sel of selectors) {
             if (sel !== selected) {
                 sel.options[selected.getAttribute("oldvalue")].hidden = false;
